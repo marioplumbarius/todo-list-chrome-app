b98fdfae3ec33f7be3038d50fd5312c5c775c216
Mario Souza <msouza@e-ig-1415.igcorp.intranet>
2013-08-05 20:49:05 -0300

    criando um TODO para refatorar o codigo atraves de OOP

---
+++ js/todo-v2.js
@@ -0,0 +1,21 @@
+Classes
+
+	Todo 
+	- irá instanciar as classes e renderizar a pagina
+	- deve ter atributos de versao e estado (running, errors, etc)
+
+	Storage
+	- será um objeto com as <ul>s da página e suas respectivas <li>s
+	- deverá ter metodos para adicionar e remover <ul> e <li>s
+	- deve ter um metodo para salvar novos dados no Storage
+	- deve ter um metodo para adicionar dados no storage.local e storage.sync
+
+	Listas
+	- deve ter um container <ul> ou <ol>
+	- deve ter funcoes de adicionar e remover <li>s
+	- deve computar a quantidade de <li>s na Lista
+
+	Item
+	- deve ser uma <li>
+	- todos seus dados devem estar expostos nos atributos do objeto
+	- deve ter um metodo para alterar seus dados
